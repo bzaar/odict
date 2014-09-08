1a1fe3ecbb128bc804395229094edfe06269a010
bzaar <slepov@gmail.com>
2014-09-08 09:00:50 +0100

    Allow for uppercase characters in lemmas.

--- odict.ru/add/DictionaryHelper.cs
+++ odict.ru/add/DictionaryHelper.cs
@@ -17,7 +17,7 @@ namespace odict.ru.add
             int StressPosition = text.IndexOf(StressMark);
 
             return StressPosition > 0 // is strees mark specified and it's position after the first letter
-                && Slepov.Russian.Syllable.LowercaseVowels.IndexOf (text[StressPosition - 1]) != -1; //previous letter has to be a vowel
+                && Slepov.Russian.Syllable.LowercaseVowels.IndexOf (char.ToLowerInvariant (text[StressPosition - 1])) != -1; //previous letter has to be a vowel
         }
 
         public static string RemoveStressMarks(string lemma)
